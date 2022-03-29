6c1659d97ff45a476b6e187e7f4e445ad21b6314
ldbk <laurent.dubroca@gmail.com>
2022-03-29 15:57:10 +0200

    AIS enfin

--- TP/statais.R
+++ TP/statais.R
@@ -11,7 +11,36 @@ str(zoneAIS)
 #a map
 ggplot()+
   geom_raster(data=zoneAIS,aes(x=x,y=y,fill=pêche))+
-  scale_fill_distiller(palette="Spectral",name="Densité de\n navires\n(h.km^2)",
-                       trans="log10")
+  scale_fill_distiller(palette="Spectral",trans="log10")
   
+ggplot()+
+  geom_raster(data=zoneAIS,aes(x=x,y=y,fill=plaisance))+
+  scale_fill_distiller(palette="Spectral",trans="log10")
+
+ggplot()+
+  geom_raster(data=zoneAIS,aes(x=x,y=y,fill=dragage))+
+  scale_fill_distiller(palette="Spectral",trans="log10")
+
+#structure du jeu de données
+str(zoneAIS)
+#selection variables
+ais<-zoneAIS[,3:7]
+#calul de distance
+dais<-dist((ais))
+#classification
+hdais<-hclust(dais,method="ward.D2")
+plot(hdais)
+rect.hclust(hdais,k=6)
+#recup les zones homogènes
+zoneAIS$zones<-cutree(hdais,6)
+
+ggplot()+
+  geom_raster(data=zoneAIS,aes(x=x,y=y,fill=zones))+
+  scale_fill_distiller(palette="Spectral")
+
+tmp<-zoneAIS%>%pivot_longer(pêche:plaisance)%>%
+  mutate(zones=as.factor(zones))
+ggplot()+
+  geom_boxplot(data=tmp,aes(x=zones,y=value,fill=zones))+
+  facet_wrap(~name,scale="free_y")
 
